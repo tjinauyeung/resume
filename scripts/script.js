72250ee17d17a5a2bd404ea24975c77e18edde77
Tjin Au Yeung <tjinauyeung@gmail.com>
2016-03-07 23:45:47 +0100

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+	$(".container").delay(1000).animate({opacity: '1', marginTop: '-=40px'}, 700);
+	$("footer").delay(1000).animate({opacity: '1', marginTop: '-=40px'}, 700);
+})
